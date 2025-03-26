a2f391303341a1bc06354f48ca0de94b9b39368f
Gnana-Pravallika <GnanaPravallika.Pakanati@cognizant.com>
2025-03-26 10:01:02 +0530

    AppointmentModel.java

--- src/main/java/com/example/healthcare/Model/AppointmentModel.java
+++ src/main/java/com/example/healthcare/Model/AppointmentModel.java
@@ -1,5 +1,50 @@
 package com.example.healthcare.Model;
-
+import org.springframework.stereotype.Component;
+@component
 public class AppointmentModel {
+	private String appointmentID;
+    private String patientID;
+    private String doctorID;
+    private String timeSlot;
+    private String status; 
 
+    public String getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(String appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getDoctorID() {
+        return doctorID;
+    }
+
+    public void setDoctorID(String doctorID) {
+        this.doctorID = doctorID;
+    }
+
+       public String getTimeSlot() {
+        return timeSlot;
+    }
+
+       public void setTimeSlot(String timeSlot) {
+        this.timeSlot = timeSlot;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
