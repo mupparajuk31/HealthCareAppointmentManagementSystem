7b25b7adc003fbbc8326c6ec58485d4190e4a3c4
Gnana-Pravallika <GnanaPravallika.Pakanati@cognizant.com>
2025-03-26 10:02:33 +0530

    ConsultationModel.java

--- src/main/java/com/example/healthcare/Model/ConsultationModel.java
+++ src/main/java/com/example/healthcare/Model/ConsultationModel.java
@@ -1,5 +1,41 @@
 package com.example.healthcare.Model;
-
+import org.springframework.stereotype.Component;
+@component
 public class ConsultationModel {
+	private String consultationID;
+    private String appointmentID;
+    private String notes;
+    private String prescription;
 
+    public String getConsultationID() {
+        return consultationID;
+    }
+
+    public void setConsultationID(String consultationID) {
+        this.consultationID = consultationID;
+    }
+
+    public String getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(String appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getPrescription() {
+        return prescription;
+    }
+
+    public void setPrescription(String prescription) {
+        this.prescription = prescription;
+    }
 }
