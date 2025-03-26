069c0c8299f0823e560f6d1b9d4b8c2fcc6401d5
Gnana-Pravallika <GnanaPravallika.Pakanati@cognizant.com>
2025-03-26 10:01:42 +0530

    AvailabilityModel.java

--- src/main/java/com/example/healthcare/Model/AvailabilityModel.java
+++ src/main/java/com/example/healthcare/Model/AvailabilityModel.java
@@ -1,5 +1,29 @@
 package com.example.healthcare.Model;
-
+import org.springframework.stereotype.Component;
+@component
 public class AvailabilityModel {
-
+	
+	private String doctorID;
+    private Date date;
+    private List<String> timeSlots;
+    public String getDoctorID() {
+        return doctorID;
+    }
+    public void setDoctorID(String doctorID) {
+        this.doctorID = doctorID;
+    }
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    public List<String> getTimeSlots() {
+        return timeSlots;
+    }
+    public void setTimeSlots(List<String> timeSlots) {
+        this.timeSlots = timeSlots;
+    }
+ 
+	
 }
